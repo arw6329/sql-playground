16898b605a360d3a05b09a5b5da46c54a596f35f
arw6329 <70402863+arw6329@users.noreply.github.com>
2024-12-20 16:23:59 -0800

    Fix running with ENABLED_DBS env var set to *

--- webapp/src/SQLPG/Environment/Environment.php
+++ webapp/src/SQLPG/Environment/Environment.php
@@ -6,6 +6,19 @@ class Environment {
     static function fetchEnabledDBs(): array {
         $enabledDBs = getenv('ENABLED_DBS');
 
-        return explode(',', $enabledDBs);
+        if($enabledDBs === '*') {
+            return [
+                'postgres16',
+                'postgres15',
+                'postgres14',
+                'postgres13',
+                'postgres12',
+                'mysql8.4',
+                'mysql8.0',
+                'oracle23ai'
+            ];
+        } else {
+            return explode(',', $enabledDBs);
+        }
     }
 }
